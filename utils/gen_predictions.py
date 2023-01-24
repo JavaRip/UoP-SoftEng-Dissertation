2c62d2e6c9a566ac39aa7ca4ab62e7abdd2f454f
JavaRip <up941226@myport.ac.uk>
2023-01-24 10:46:11 +0000

    prediction scripts

---
+++ utils/gen_predictions.py
@@ -0,0 +1,24 @@
+from subprocess import check_output
+import pandas as pd
+import json
+import sys
+
+def predict(filepath, stain_color):
+  cmd_arr = [
+    'node',
+    './utils/iarsenic-wrapper.js',
+    filepath,
+    stain_color,
+  ]
+
+  stdout = check_output(cmd_arr).decode(sys.stdout.encoding).replace('\n', '')
+  return pd.read_csv(stdout), stdout
+
+if __name__ == '__main__':
+  stain_color = 'Red'
+  filepath = './well_data/test.csv'
+
+  df = pd.read_csv(filepath)
+  df['predictions'], outfile = predict(filepath, stain_color)
+  df.to_csv(outfile, index=False)
+  print(f'written to {outfile}')
---
+++ utils/iarsenic-wrapper.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+const produceEstimate = require("../models/model5/estimator.js");
+
+const srcCsv = process.argv[2];
+const stainColour = process.argv[3];
+
+const rows = String(fs.readFileSync(srcCsv)).split("\n");
+predictions = [];
+
+for (const row of rows) {
+  rowArr = row.split(",");
+
+  if (rowArr[0] === "div") continue;
+  if (rowArr[0] === "") continue;
+
+  const div = rowArr[0];
+  const dis = rowArr[1];
+  const upa = rowArr[2];
+  const uni = rowArr[3];
+  const mou = rowArr[4];
+  const depth = rowArr[5];
+  const colour = stainColour;
+  const utensil = "";
+  const flood = "";
+
+  const divisions = JSON.parse(
+    fs.readFileSync(`./models/model5/aggregate-data/${div}-${dis}.json`)
+  );
+
+  const estimate = produceEstimate(
+    divisions,
+    div,
+    dis,
+    upa,
+    uni,
+    mou,
+    depth,
+    colour,
+    utensil,
+    flood
+  );
+
+  if (estimate.severity == null) {
+    predictions.push("none");
+  } else {
+    predictions.push(estimate.severity);
+  }
+}
+
+// add header
+predictions.unshift("prediction");
+
+const outFilename = `./prediction_data/model5-${stainColour}-${Math.floor(
+  new Date().getTime() / 1000
+)}.csv`;
+fs.writeFileSync(outFilename, predictions.join("\n"));
+console.log(outFilename);
